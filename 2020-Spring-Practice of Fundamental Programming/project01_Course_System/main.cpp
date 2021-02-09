86caf49ed098265819cc7e9b157617ae89d3e224
stephenchieh <63652929+stephenchieh@users.noreply.github.com>
2021-02-09 16:39:32 +0800

    Add files via upload

---
+++ 2020-Spring-Practice of Fundamental Programming/project01_Course_System/main.cpp
@@ -0,0 +1,21 @@
+#include"login.h"
+#include<cstdlib>
+#include<iostream>
+using namespace std;
+#include<fstream>
+#include<string>
+int main()
+{
+	string ss;
+	cin >> ss;
+	cout << ss;
+	/*string ss = "请问e1";
+	int k = 0;
+	for (int i = 0;i < 6;++i)
+		k += sizeof(ss[i]);
+	cout << k << " " << sizeof("请问e1");*/
+	//system("title 学生选课信息管理系统");
+	//system("mode con cols=110 lines=40");//窗口宽度高度
+	//welcome_ui();
+	return 0;
+}
